14c43c2ab53511e3ef582b73d8b3570d63690434
Graham Ballantyne <grahamb@sfu.ca>
2016-03-20 22:51:36 -0700

    Use env vars in server/index

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 /* eslint no-console: 0 */
 
+import 'dotenv/config'
 import express from 'express'
 import bodyParser from 'body-parser'
 import session from 'express-session'
@@ -8,15 +9,25 @@ import {
   authenticateUser,
   handleSingleSignout
 } from './auth-middleware'
+const RedisStore = require('connect-redis')(session)
 
-const store = new session.MemoryStore()
 const app = express()
-app.use(session({
-  secret: 'wharrrgarbl',
+
+let sessionConfig = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
   resave: false,
   saveUninitialized: false,
-  store
-}))
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1)
+  sessionConfig.cookie.secure = true
+  sessionConfig.store = new RedisStore({
+    url: process.env.REDIS_URL
+  })
+}
+app.use(session(sessionConfig))
 
 app.get('/auth', authenticateUser)
 
@@ -26,6 +37,6 @@ app.get('*', loggedin, (req, res) => {
   res.send(`<a href="https://cas.sfu.ca/cas/logout">Logout</a>`)
 })
 
-app.listen(3000, () => {
-  console.info('server listening on 3000')
+app.listen(process.env.EXPRESS_PORT, () => {
+  console.info(`server listening on ${process.env.EXPRESS_PORT}`)
 })
