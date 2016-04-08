6d9191dd40daed41377ef82aa5201b48e6a5a551
Graham Ballantyne <grahamb@sfu.ca>
2016-04-07 17:01:05 -0700

    Add memory-backed PGT Store

---
+++ src/server/pgt-store/memory.js
@@ -0,0 +1,25 @@
+export default class MemoryStore {
+  constructor() {
+    this.store = new Map()
+  }
+
+  get(pgtiou) {
+    return this.store.get(pgtiou)['pgt']
+  }
+
+  set(pgtiou, pgt) {
+    this.store.set(pgtiou, { pgt })
+  }
+
+  all() {
+    return this.store.entries()
+  }
+
+  del(pgtiou) {
+    this.store.delete(pgtiou)
+  }
+
+  clear() {
+    this.store.clear()
+  }
+}
