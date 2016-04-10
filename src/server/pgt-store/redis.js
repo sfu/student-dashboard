692c623f47c990023ac6f5546e99acf6adc0f533
Graham Ballantyne <grahamb@sfu.ca>
2016-04-10 10:17:29 -0700

    Add PGT RedisStore

---
+++ src/server/pgt-store/redis.js
@@ -0,0 +1,52 @@
+export default class RedisStore {
+  constructor(client = undefined, options = {
+    prefix: 'PGT:::',
+    expiry: 3600 // seconds
+  }) {
+    if (!client) {
+      throw new Error('You must supply a Redis client to RedisStore')
+    }
+    this.client = client
+    this.prefix = options.prefix
+    this.expiry = options.expiry
+  }
+
+  get(pgtiou) {
+    const key = `${this.prefix}${pgtiou}`
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  }
+
+  set(pgtiou, pgt) {
+    const key = `${this.prefix}${pgtiou}`
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, this.expiry, pgt, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  }
+
+  del(pgtiou) {
+    const key = `${this.prefix}${pgtiou}`
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  }
+}
