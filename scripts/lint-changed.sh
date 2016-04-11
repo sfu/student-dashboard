0b0f1cd687e019b52b7482038ab059763391989b
Graham Ballantyne <grahamb@sfu.ca>
2016-04-11 10:58:02 -0700

    Add lint-changed.sh script
    Runs eslint against files staged for commit. Called by git-precommit.

---
+++ scripts/lint-changed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+IFS=$'\n'
+changed=($(git --no-pager diff --cached --name-only --diff-filter=ACMR | grep .js$))
+
+if [ ${#changed[@]} -gt 0 ]; then
+  node_modules/.bin/eslint -c .eslintrc $changed
+fi
