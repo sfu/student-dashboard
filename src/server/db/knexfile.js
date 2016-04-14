d8fb1c18fbf349cb25e41bb805bcb2ae1592af0a
Graham Ballantyne <grahamb@sfu.ca>
2016-04-14 14:56:53 -0700

    Add knexfile

---
+++ src/server/db/knexfile.js
@@ -0,0 +1,21 @@
+// require('dotenv').config({
+//   path: '../../../.env'
+// })
+
+if (!(process.env.PG_HOST || process.env.PG_USER || process.env.PG_PASS || process.env.PG_DATABASE)) {
+  throw new Error('Required DB options not set in .env')
+}
+
+module.exports = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    database: process.env.PG_DATABASE
+  },
+  migrations: {
+    directory: './migrations',
+    tableName: 'schema_migrations'
+  }
+}
