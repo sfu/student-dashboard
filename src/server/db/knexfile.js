ecf4d92d92495fceacbff0c8348bf1e58c760e09
Graham Ballantyne <grahamb@sfu.ca>
2016-04-18 16:49:38 -0700

    Workaround to require dotenv when loading knex CLI
    Rather than having a require('dotenv').config() call inside knexfile.js, require it when loading the CLI. It's a bit hacky, as it needs to invoke `node node_modules/.bin/knex`, passing in the -r flag, but it works.

--- src/server/db/knexfile.js
+++ src/server/db/knexfile.js
@@ -1,7 +1,3 @@
-// require('dotenv').config({
-//   path: '../../../.env'
-// })
-
 if (!(process.env.PG_HOST || process.env.PG_USER || process.env.PG_PASS || process.env.PG_DATABASE)) {
   throw new Error('Required DB options not set in .env')
 }
