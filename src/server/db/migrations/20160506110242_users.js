97d47a645e266991f6b4d439fadb5f93d55c874f
Graham Ballantyne <grahamb@sfu.ca>
2016-05-06 11:14:15 -0700

    Default updated_at to now()

--- src/server/db/migrations/20160506110242_users.js
+++ src/server/db/migrations/20160506110242_users.js
@@ -10,7 +10,7 @@ exports.up = (knex) => {
     t.text('access_token').nullable().comment(`The user''s oAuth access token`)
     t.text('refresh_token').nullable().comment(`The user''s oAuth refresh token`)
     t.dateTime('created_at').notNull().defaultTo(knex.raw('now()'))
-    t.dateTime('updated_at').notNull()
+    t.dateTime('updated_at').notNull().defaultTo(knex.raw('now()'))
   })
 }
 
