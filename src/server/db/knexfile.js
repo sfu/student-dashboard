7bc75da6edd775df51a02bac19451c9851377154
Graham Ballantyne <grahamb@sfu.ca>
2016-05-06 09:35:45 -0700

    use DATABASE_URL instead of discreet PG_* vars

--- src/server/db/knexfile.js
+++ src/server/db/knexfile.js
@@ -1,17 +1,12 @@
 const path = require('path')
 
-if (!(process.env.PG_HOST || process.env.PG_USER || process.env.PG_PASS || process.env.PG_DATABASE)) {
+if (!process.env.DATABASE_URL) {
   throw new Error('Required DB options not set in .env')
 }
 
 module.exports = {
   client: 'postgresql',
-  connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASS,
-    database: process.env.PG_DATABASE
-  },
+  connection: process.env.DATABASE_URL,
   migrations: {
     directory: path.resolve(__dirname, './migrations'),
     tableName: 'schema_migrations'
