1142ce91d13bd03510e07fd7d623032be6e28cf8
Graham Ballantyne <grahamb@sfu.ca>
2016-05-09 10:45:27 -0700

    Add environment.js
    .env file loader. Loads a .env.${NODE_ENV} file located at the root of the project. Configured for silent running. Useful in --require hooks – see the `start` and `test` scripts in package.json for examples.

---
+++ environment.js
@@ -0,0 +1,9 @@
+// environment.js
+// This file loads a .env.${NODE_ENV} file from the same directory as this file
+// If NODE_ENV === undefined, assume development
+// Use in require hooks -- see the `start` and `test` scripts in package.json
+
+const resolve = require('path').resolve
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const path = resolve(__dirname, `.env.${NODE_ENV}`)
+const dotenv = require('dotenv').load({silent: true, path}) // eslint-disable-line no-unused-vars
