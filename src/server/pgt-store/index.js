e90056a493357fd2b0cc2b2a4e78f9694ccccc88
Graham Ballantyne <grahamb@sfu.ca>
2016-05-20 14:24:54 -0700

    Update babel config to minimum required for node6 + async

--- src/server/pgt-store/index.js
+++ src/server/pgt-store/index.js
@@ -1,2 +1,2 @@
-export MemoryStore from './memory'
-export RedisStore from './redis'
+export {default as MemoryStore} from './memory'
+export {default as RedisStore} from './redis'
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,3 +1,3 @@
-export app from './app'
-export auth from './auth'
-export pgt from './pgt'
+export {default as app} from './app'
+export {default as auth} from './auth'
+export {default as pgt} from './pgt'
