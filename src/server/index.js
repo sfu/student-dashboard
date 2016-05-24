61117bcc3e7084d7303634bea92ef421d82db781
Graham Ballantyne <grahamb@sfu.ca>
2016-05-24 14:59:58 -0700

    Simplify server/index.js
    Get rid of a lot of the configuration options and assume some defaults:
    
    - redis will be used and available for caching
    - server will be running HTTP, behind a HTTPS proxy/LB (see: cookie settings, trust proxy setting)

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,6 @@
 /* eslint no-console: 0 */
 import express from 'express'
 import session from 'express-session'
-import fs from 'fs'
-import http from 'http'
-import https from 'https'
 import helmet from 'helmet'
 import path from 'path'
 import {sync as uid} from 'uid-safe'
@@ -16,9 +13,14 @@ const app = express()
 let sessionConfig = {
   secret: process.env.SESSION_SECRET,
   name: process.env.SESSION_COOKIE_NAME || 'connect.sid',
-  cookie: {},
+  cookie: {
+    secure: true
+  },
   resave: false,
   saveUninitialized: false,
+  store: new RedisStore({
+    url: process.env.SESSION_STORE_REDIS_URL
+  }),
   genid(req) {
     if (req.query && req.query.ticket) {
       return `cas_session:::${req.query.ticket}`
@@ -28,18 +30,7 @@ let sessionConfig = {
   }
 }
 
-// assume that if we're not serving HTTPS, we're behind a proxy
-if (!process.env.EXPRESS_HTTPS) {
-  app.set('trust proxy', 1)
-  sessionConfig.cookie.secure = true
-}
-
-if (process.env.SESSION_STORE_REDIS_URL) {
-  sessionConfig.store = new RedisStore({
-    url: process.env.SESSION_STORE_REDIS_URL
-  })
-}
-
+app.set('trust proxy', 1)
 app.use(session(sessionConfig))
 app.use(helmet())
 app.use(express.static(path.resolve(__dirname, '../../public')))
@@ -54,23 +45,6 @@ app.use((err, req, res, next) => {  // eslint-disable-line no-unused-vars
   res.status(500).send(err)
 })
 
-if (process.env.EXPRESS_HTTPS) {
-  if (process.env.HTTPS_CERT_FILE && process.env.HTTPS_KEY_FILE) {
-    let httpsOptions = {
-      cert: fs.readFileSync(process.env.HTTPS_CERT_FILE),
-      key: fs.readFileSync(process.env.HTTPS_KEY_FILE)
-    }
-    if (process.env.HTTPS_CA_BUNDLE) {
-      httpsOptions.ca = process.env.HTTPS_CA_BUNDLE.split(',').map(cert => fs.readFileSync(cert))
-    }
-    https.createServer(httpsOptions, app).listen(process.env.EXPRESS_PORT, () => {
-      console.info(`HTTPS server listening on port ${process.env.EXPRESS_PORT}`)
-    })
-  } else {
-    throw 'EXPRESS_HTTPS was specified in your .env, but no HTTPS_CERT_FILE and/or HTTPS_KEY_FILE was specified.'
-  }
-} else {
-  http.createServer(app).listen(process.env.EXPRESS_PORT, () => {
-    console.info(`HTTP server listening on port ${process.env.EXPRESS_PORT}`)
-  })
-}
+app.listen(process.env.EXPRESS_PORT, () => {
+  console.info(`HTTP server listening on port ${process.env.EXPRESS_PORT}`)
+})
