a792922e9f5007b903f848161e92f5968f762b4a
Graham Ballantyne <grahamb@sfu.ca>
2016-05-24 14:53:03 -0700

    TEMP: send HTML document on requests to /

--- src/server/routes/app.js
+++ src/server/routes/app.js
@@ -4,7 +4,25 @@ import {loggedin} from '../auth-middleware'
 const router = Router()
 
 router.get('/', loggedin, (req, res) => {
-  res.status(200).send('OK From Router')
+  const html = dedent`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charSet="utf-8"/>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+      <meta name="viewport" content="user-scalable=no,initial-scale=1.0,maximum-scale=1.0,width=device-width"/>
+      <meta name="timestamp" content="${(new Date).toISOString()}"/>
+      <title>{title}</title>
+    </head>
+
+    <body>
+      <h1>Hello Static</h1>
+      <div id="app"/>
+      <script src="assets/app.js"></script>
+    </body>
+  </html>
+  `
+  res.send(html).status(200)
 })
 
 export default router
