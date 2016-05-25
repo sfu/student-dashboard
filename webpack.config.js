c35fab87e6681304a418518be5c9c2c0ce8c5ce4
Graham Ballantyne <grahamb@sfu.ca>
2016-05-25 15:38:06 -0700

    Add webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    'webpack/hot/dev-server',
+    `webpack-dev-server/client?https://icat-graham.its.sfu.ca`,
+    path.resolve(__dirname, 'src/client/home.js')
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public/assets'),
+    publicPath: '/assets/'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: ['es2015', 'react', 'react-hmre'] } }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
+}
