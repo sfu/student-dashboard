ad4de77b292bcc6a643dddd2d563f91c077ffb8e
Graham Ballantyne <grahamb@sfu.ca>
2016-06-16 16:23:04 -0700

    Delete unused test

--- server/auth-middleware/__test__/authenticateUser.js
+++ server/auth-middleware/__test__/authenticateUser.js
@@ -4,17 +4,7 @@ import {mockReq, mockRes} from 'sinon-express-mock'
 import {authenticateCasUser} from '../index'
 import cas from '../../cas-client'
 
-test('Calling with a user sesison calls `next`', t => {
-  const next = sinon.spy()
-  const req = mockReq({
-    session: { auth: { status: true } }
-  })
-  const res = mockRes()
-  authenticateCasUser(req, res, next)
-  t.true(next.calledOnce)
-})
-
-test('Calling without a user session calls `cas.authenticate`', t => {
+test('Calling authenticateCasUser calls `cas.authenticate`', t => {
   const next = sinon.spy()
   const req = mockReq()
   const res = mockRes({ redirect: sinon.spy() })
