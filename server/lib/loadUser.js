8bde7140e8fdb9e9a545adce095bc26cadd7d1ca
Graham Ballantyne <grahamb@sfu.ca>
2016-06-16 13:39:52 -0700

    Remove stray console.log

--- server/lib/loadUser.js
+++ server/lib/loadUser.js
@@ -5,7 +5,6 @@ export default async function loadUser(username, fields = '*') {
     const result = await db('users').select(fields).where({username})
     return result.length ? result[0] : null
   } catch(e) {
-    console.log('THROWING')
     throw(e)
   }
 }
