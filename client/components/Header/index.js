6bee0566635257dd3ab7e51ace7f0b09836bff26
Graham Ballantyne <grahamb@sfu.ca>
2016-08-22 17:43:41 -0700

    Add Header component

---
+++ client/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import snapLogo from './sfusnap.png'
+import menu from './menu.svg'
+
+const styles = {
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottom: '2px solid #A6192E'
+  },
+  h1: {
+    color: '#A6192E',
+    fontWeight: '300'
+  }
+}
+
+export const Header = () => {
+  return (
+    <header style={styles.header}>
+      <img alt="SFU Snap Logo" src={snapLogo} height={50} width={50} />
+      <h1 style={styles.h1}>Dashboard</h1>
+      <img alt="menu" src={menu} />
+    </header>
+  )
+}
