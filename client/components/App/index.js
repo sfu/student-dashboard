8c9a11eb9c5ab81cd59a89d6fb606573474f269e
Graham Ballantyne <grahamb@sfu.ca>
2016-08-23 17:33:59 -0700

    AntiAlias all the things

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -4,7 +4,8 @@ import {Header} from 'components/Header' // eslint-disable-line
 import {HelloTile} from 'components/HelloTile'  // eslint-disable-line
 
 const appStyle = {
-  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  WebkitFontSmoothing: 'antialiased'
 }
 
 export const _App = ({viewer}) => {
