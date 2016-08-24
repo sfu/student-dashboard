b556859d8ddbb5e801cf2655480a2c16dde57fb7
Graham Ballantyne <grahamb@sfu.ca>
2016-08-23 17:22:16 -0700

    Add logout route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,4 +29,10 @@ router.post('/login/cas',
   handleSingleSignout
 )
 
+router.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/')
+  })
+})
+
 export default router
