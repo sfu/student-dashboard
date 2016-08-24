d4e5e0f8fccbd3bf0790d1945c8b31a58344ebf3
Graham Ballantyne <grahamb@sfu.ca>
2016-08-23 17:33:49 -0700

    Add <HelloTile> to <App>

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -1,6 +1,7 @@
 import {default as React, PropTypes} from 'react'
 import Relay from 'react-relay'
 import {Header} from 'components/Header' // eslint-disable-line
+import {HelloTile} from 'components/HelloTile'  // eslint-disable-line
 
 const appStyle = {
   fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
@@ -10,6 +11,7 @@ export const _App = ({viewer}) => {
   return (
   <div style={appStyle}>
     <Header />
+    <HelloTile names={viewer.names} />
   </div>
   )
 }
@@ -25,6 +27,8 @@ export const App = Relay.createContainer(_App, {
   fragments: {
     viewer: () => Relay.QL`
       fragment on ViewerType {
+        names:userBio(userid: $userid) {
+          ${HelloTile.getFragment('names')}
         }
       }
     `
