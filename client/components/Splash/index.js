b324718c835da77f36128d7afb4d20392ccc0b62
Graham Ballantyne <grahamb@sfu.ca>
2016-08-29 15:16:32 -0700

    Use CSS Modules for Splash component

--- client/components/Splash/index.js
+++ client/components/Splash/index.js
@@ -1,31 +1,11 @@
 import {default as React, PropTypes} from 'react'
 import logo from './sfusnap_white.png'
-
-
-const divStyle={
-  backgroundColor: '#A6192E',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  height: '100vh',
-  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-  WebkitFontSmoothing: 'antialiased',
-  color: '#fff',
-  fontSize: '1.5em',
-  textAlign: 'center'
-}
-
-const imgStyle = {
-  borderRadius: '5px',
-  width: '150px',
-  height: '150px'
-}
+import styles from './Splash.css'
 
 export const Splash = ({children}) => {
   return (
-    <div style={divStyle}>
-      <img alt="SFU Snap" src={logo} style={imgStyle}/>
+    <div className={styles.splash}>
+      <img alt="SFU Snap" src={logo} className={styles.logo}/>
       {children}
     </div>
   )
