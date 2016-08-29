e755b068880f4a187ed5a80b438d7159a156bf4d
Graham Ballantyne <grahamb@sfu.ca>
2016-08-29 15:16:47 -0700

    Use CSS Modules for HelloTile component

--- client/components/HelloTile/index.js
+++ client/components/HelloTile/index.js
@@ -1,36 +1,6 @@
 import {default as React, PropTypes} from 'react'
 import Relay from 'react-relay'
-
-const divStyle = {
-  backgroundColor: '#007096',
-  color: '#FFFFFF',
-  padding: '25px',
-}
-
-const pStyle = {
-  margin: '0px',
-  marginBottom: '1em',
-  fontSize: '1.25em',
-  fontWeight: '300'
-}
-
-const buttonStyle = {
-  container: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row-reverse'
-  },
-  button: {
-    backgroundColor: '#A6192E',
-    borderRadius: '5px',
-    color: '#FFFFFF',
-    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-    fontSize: '1.25em',
-    fontWeight: '500',
-    border: 'none',
-    padding: '.25em 1em'
-  }
-}
+import styles from './HelloTile.css'
 
 export const _HelloTile = React.createClass({
   propTypes: {
@@ -55,13 +25,13 @@ export const _HelloTile = React.createClass({
     const {names} = this.props
     const name = names.commonname ? names.commonname : names.firstnames
     return this.state.hide ? null : (
-      <div style={divStyle}>
-        <p style={pStyle}>Hello, <b>{name}</b>.</p>
-        <p style={pStyle}>You have <b>?? class</b> and <b>?? assignments</b> due today.</p>
-        <div style={buttonStyle.container}>
+      <div className={styles.helloTile}>
+        <p className={styles.p}>Hello, <b>{name}</b>.</p>
+        <p className={styles.p}>You have <b>?? class</b> and <b>?? assignments</b> due today.</p>
+        <div className={styles.buttonContainer}>
         <button
           onClick={this.toggleHide}
-          style={buttonStyle.button}
+          className={styles.button}
         >Got it!</button>
         </div>
       </div>
