677816ae9978356c9dccc20b75eefba722364e5a
Graham Ballantyne <grahamb@sfu.ca>
2016-08-29 17:00:11 -0700

    Add Widget Component
    <Widget> is intended to be a container for individual widgets composed as children. Ex:
    
    // SampleWidget.js
    export const SampleWidget = () => {
      return (
        <div>
          <p>I am a sample widget!</p>
        </p>
      )
    }
    
    // App.js
    
    import SampleWidget from './SampleWidget'
    
    <Widget title="Sample Widget">
      <SampleWidget />
    </Widget>
    
    ... renders:
    
    <div>
      <h1>Sample Widget</h1>
      <div>
        <div> <!-- {children} -->
          <p>I am a sample widget!</p>
        </div>
      </div>
    <div>

---
+++ client/components/Widget/index.js
@@ -0,0 +1,16 @@
+import {default as React, PropTypes} from 'react'
+import styles from './Widget.css'
+
+export const Widget = ({title, children}) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.widget}>{children}</div>
+    </div>
+  )
+}
+
+Widget.propTypes = {
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string.isRequired
+}
