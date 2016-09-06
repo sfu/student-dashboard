90125d3bc169245ee46f7b0ba24a9203df1d94df
Graham Ballantyne <grahamb@sfu.ca>
2016-09-06 16:52:07 -0700

    Dynamically display the "You have..." string

--- client/components/HelloTile/index.js
+++ client/components/HelloTile/index.js
@@ -1,10 +1,13 @@
 import {default as React, PropTypes} from 'react'
 import Relay from 'react-relay'
+import plural from 'utils/plural'
+import moment from 'moment'
 import styles from './HelloTile.css'
 
 export const _HelloTile = React.createClass({
   propTypes: {
-    names: PropTypes.object
+    names: PropTypes.object,
+    schedule: PropTypes.object
   },
 
   getInitialState() {
@@ -17,17 +20,33 @@ export const _HelloTile = React.createClass({
     const nextHide = !this.state.hide
     this.setState({
       hide: nextHide
-    }, () => {
     })
   },
 
   render() {
-    const {names} = this.props
+    const {names, schedule} = this.props
     const name = names.commonname ? names.commonname : names.firstnames
+    const today = moment().day()
+    const stats = {
+      classes: schedule.edges.nodes.filter(item => moment(item.start_at).day() === today && item.type === 'class').length,
+      assignments: schedule.edges.nodes.filter(item => moment(item.start_at).day() === today && item.type === 'assignment').length
+    }
+
+    let youHave
+    if (stats.classes === 0 && stats.assignment === 0) {
+      youHave = <span>You have nothing scheduled for today</span>
+    } else if (stats.classes > 0 && stats.assignments === 0) {
+      youHave = <span>You have <b>{stats.classes} {plural('class', stats.classes)}</b> today.</span>
+    } else if (stats.classes === 0 && stats.assignmets > 0) {
+      youHave = <span>You have <b>{plural('assignment'), stats.assignments}</b> due today.</span>
+    } else {
+      youHave = <span>You have <b>{stats.classes} {plural('class', stats.classes)}</b> and <b>{stats.assignments} {plural('assignment', stats.assignments)}</b> due today.</span>
+    }
+
     return this.state.hide ? null : (
       <div className={styles.helloTile}>
         <p className={styles.p}>Hello, <b>{name}</b>.</p>
-        <p className={styles.p}>You have <b>?? class</b> and <b>?? assignments</b> due today.</p>
+        <p className={styles.p}>{youHave}</p>
         <div className={styles.buttonContainer}>
         <button
           onClick={this.toggleHide}
