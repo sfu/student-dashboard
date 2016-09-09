976818840872577644d941a9f59679d60133791c
Graham Ballantyne <grahamb@sfu.ca>
2016-09-08 17:20:04 -0700

    Ditch the plural library; regigger youHave in HelloTile
    Plural lib was causing all sorts of bizarre react rendering errors.

--- client/components/HelloTile/index.js
+++ client/components/HelloTile/index.js
@@ -1,6 +1,5 @@
 import {default as React, PropTypes} from 'react'
 import Relay from 'react-relay'
-import plural from 'utils/plural'
 import moment from 'moment'
 import styles from './HelloTile.css'
 
@@ -23,30 +22,33 @@ export const _HelloTile = React.createClass({
     })
   },
 
+  youHave(data) {
+    const today = moment().day()
+    const stats = {
+      classes: data.filter(item => moment(item.start_at).day() === today && item.type === 'class').length,
+      assignments: data.filter(item => moment(item.start_at).day() === today && item.type === 'assignment').length
+    }
+    const classes = stats.classes === 1 ? 'class' : 'classes'
+    const assignments = stats.classes === 1 ? 'assignment' : 'assignments'
+    if (stats.classes === 0 && stats.assignment === 0) {
+      return <span>You have nothing scheduled for today.</span>
+    } else if (stats.classes > 0 && stats.assignments === 0) {
+      return <span>You have <b>{stats.classes} {classes}</b> today.</span>
+    } else if (stats.classes === 0 && stats.assignmets > 0) {
+      return <span>You have <b>{stats.assignments} {assignments}</b> due today.</span>
+    } else {
+      return <span>You have <b>{stats.classes} {classes}</b> and <b>{stats.assignments} {assignments}</b> due today.</span>
+    }
+  },
+
   render() {
     const {names, schedule} = this.props
     const name = names.commonname ? names.commonname : names.firstnames
-    const today = moment().day()
-    const stats = {
-      classes: schedule.edges.nodes.filter(item => moment(item.start_at).day() === today && item.type === 'class').length,
-      assignments: schedule.edges.nodes.filter(item => moment(item.start_at).day() === today && item.type === 'assignment').length
-    }
-
-    let youHave
-    if (stats.classes === 0 && stats.assignment === 0) {
-      youHave = <span>You have nothing scheduled for today</span>
-    } else if (stats.classes > 0 && stats.assignments === 0) {
-      youHave = <span>You have <b>{stats.classes} {plural('class', stats.classes)}</b> today.</span>
-    } else if (stats.classes === 0 && stats.assignmets > 0) {
-      youHave = <span>You have <b>{plural('assignment'), stats.assignments}</b> due today.</span>
-    } else {
-      youHave = <span>You have <b>{stats.classes} {plural('class', stats.classes)}</b> and <b>{stats.assignments} {plural('assignment', stats.assignments)}</b> due today.</span>
-    }
 
     return this.state.hide ? null : (
       <div className={styles.helloTile}>
-        <p className={styles.p}>Hello, <b>{name}</b>.</p>
-        <p className={styles.p}>{youHave}</p>
+        <p className={styles.p}>Hello, <b>{name}</b>!</p>
+        <p className={styles.p}>{this.youHave(schedule.edges.nodes)}</p>
         <div className={styles.buttonContainer}>
         <button
           onClick={this.toggleHide}
