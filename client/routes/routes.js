f99c29ed3c1f89f529c5be8b85d8735fbcceed9e
Graham Ballantyne <grahamb@sfu.ca>
2016-09-12 13:36:33 -0700

    Remove slash from nested route paths

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -8,9 +8,9 @@ import ViewerQueries from 'queries/ViewerQueries'
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Dashboard} queries={ViewerQueries} />
-    <Route path="/courses" component={() => <h1>Courses</h1>} />
-    <Route path="/transit" component={() => <h1>Transit</h1>} />
-    <Route path="/library" component={() => <h1>Library</h1>} />
-    <Route path="/room_finder" component={() => <h1>Room Finder</h1>} />
+    <Route path="courses" component={() => <h1>Courses</h1>} />
+    <Route path="transit" component={() => <h1>Transit</h1>} />
+    <Route path="library" component={() => <h1>Library</h1>} />
+    <Route path="room_finder" component={() => <h1>Room Finder</h1>} />
   </Route>
 )
