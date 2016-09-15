e53533517f218e6df9121f0d391c4ba0f03ee2d3
Graham Ballantyne <grahamb@sfu.ca>
2016-09-15 10:48:18 -0700

    Fix IndexLink active status in NavBar

--- client/components/NavBar/index.js
+++ client/components/NavBar/index.js
@@ -1,11 +1,11 @@
-import { default as React, PropTypes } from 'react'
+import React from 'react'
 import { NavLink } from 'components/NavLink'
 import styles from './NavBar.css'
 
 export const NavBar = () => (
   <nav className={styles.navbar} >
   <ul>
-    <li><NavLink to="/">Dashboard</NavLink></li>
+    <li><NavLink onlyActiveOnIndex={true} to="/">Dashboard</NavLink></li>
     <li><NavLink to="/courses">Courses</NavLink></li>
     <li><NavLink to="/transit">Transit</NavLink></li>
     <li><NavLink to="/library">Library</NavLink></li>
