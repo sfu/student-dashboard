a1f90fe89bd7869ec0a3635e05d6d73c640ea10d
Graham Ballantyne <grahamb@sfu.ca>
2016-10-17 15:36:48 -0700

    Add readHtmlFile to read HTML file from right place
    In production, reads from disk in public/assets
    In development, reads from webpack-dev-server's in-memory FS

---
+++ server/lib/readHtmlFile.js
@@ -0,0 +1,28 @@
+import { readFile } from 'fs'
+
+export default (filename, app) => {
+  return new Promise((resolve, reject) => {
+    if (process.env.NODE_ENV === 'production') {
+      // if in production, read the file from disk
+      readFile(`${app.get('htmlDirectory')}/${filename}`, (err, content) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(content)
+        }
+      })
+
+    } else {
+      // in in development, read the file from memory-fs
+      const compiler = app.get('compiler')
+      compiler.outputFileSystem.readFile(`${compiler.outputPath}/${filename}`, (err, content) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(content)
+        }
+      })
+    }
+
+  })
+}
--- server/routes/app.js
+++ server/routes/app.js
@@ -1,10 +1,17 @@
-import {Router} from 'express'
-import {loggedin} from '../auth-middleware'
-
+import { Router } from 'express'
+import { loggedin } from '../auth-middleware'
+import { readHtmlFile } from '../lib'
 const router = Router()
 
-router.get('/', loggedin, (req, res) => {
-  res.sendFile(`${req.app.get('htmlDirectory')}/snap.html`)
+router.get('/', loggedin, async (req, res) => {
+  try {
+    const html = await readHtmlFile('snap.html', req.app)
+    res.set('content-type','text/html')
+    res.send(html)
+    res.end()
+  } catch(e) {
+    return res.boom.badImplementation(e)
+  }
 })
 
 export default router
