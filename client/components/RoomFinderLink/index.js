f6683f176578882e8f507a11ef98acfb00fd7ae6
Graham Ballantyne <grahamb@sfu.ca>
2016-10-21 12:14:19 -0700

    Add RoomFinderLink component

---
+++ client/components/RoomFinderLink/index.js
@@ -0,0 +1,18 @@
+import { default as React, PropTypes } from 'react'
+import { Link } from 'react-router'
+
+export const RoomFinderLink = ({building, room}) =>
+  <Link
+    to={{
+      pathname: '/room_finder',
+      query: { building, room }
+    }}
+  >
+    {building} {room}
+  </Link>
+
+
+RoomFinderLink.propTypes = {
+  building: PropTypes.string.isRequired,
+  room: PropTypes.string.isRequired
+}
