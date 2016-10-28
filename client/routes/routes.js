ce9a463fc6a5f22dcff9af2efd9345529dabda20
Graham Ballantyne <grahamb@sfu.ca>
2016-10-28 16:08:37 -0700

    Replace stub with Courses component

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -3,6 +3,7 @@ import {Route, IndexRoute} from 'react-router'
 
 import { App } from 'components/App'
 import { Dashboard } from 'components/Dashboard'
+import { Courses } from 'components/Courses'
 import { Library } from 'components/Library'
 import { LibraryBarcode } from 'components/LibraryBarcode'
 import RoomFinder from 'components/RoomFinder'
@@ -12,7 +13,7 @@ import ViewerQueries from 'queries/ViewerQueries'
 export default (
   <Route path="/" component={App}>
     <IndexRoute title="Dashboard" component={Dashboard} queries={ViewerQueries} />
-    <Route title="Courses" path="courses" component={() => <h1>Courses</h1>} />
+    <Route title="Courses" path="courses" component={Courses} queries={ViewerQueries} />
     <Route title="Transit" path="transit" component={() => <h1>Transit</h1>} />
     <Route title="Library" path="library" component={Library} queries={ViewerQueries}/>
     <Route title="Library Barcode" path="library/barcode" fullScreen={true} component={LibraryBarcode} />
