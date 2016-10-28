25a4679ec87f77147311333208b3f0c33bb895b2
Graham Ballantyne <grahamb@sfu.ca>
2016-10-28 10:55:28 -0700

    Add PagerHeader component

---
+++ client/components/PagerHeader/index.js
@@ -0,0 +1,40 @@
+import {default as React, PropTypes} from 'react'
+import styles from './PagerHeader.css'
+
+const PagerHeader = ({
+  title,
+  forwardDisabled = false,
+  backDisabled = false,
+  buttonHandler,
+  currentPage
+}) => {
+  return (
+    <div className={styles.header}>
+      <button
+        disabled={backDisabled}
+        className={styles.pagerButton}
+        onClick={() => {buttonHandler(currentPage - 1)}}
+        >
+          &lt;
+        </button>
+      <span className={styles.pagerTitle}>{title}</span>
+      <button
+        disabled={forwardDisabled}
+        className={styles.pagerButton}
+        onClick={() => {buttonHandler(currentPage + 1)}}
+      >
+        &gt;
+      </button>
+    </div>
+  )
+}
+
+PagerHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+  forwardDisabled: PropTypes.bool.isRequired,
+  backDisabled: PropTypes.bool.isRequired,
+  buttonHandler: PropTypes.func.isRequired,
+  currentPage: PropTypes.number.isRequired
+}
+
+export default PagerHeader
