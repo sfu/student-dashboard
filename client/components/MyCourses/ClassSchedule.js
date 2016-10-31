f5d6671b43a3a0d230bc14db670ddc930ceccfb8
Graham Ballantyne <grahamb@sfu.ca>
2016-10-31 12:33:32 -0700

    Format time display
    Start and end times come in the data as 24h strings (e.g 1530, 0920). Left-pad them to four digits with a leading zero, and format with moment.

--- client/components/MyCourses/ClassSchedule.js
+++ client/components/MyCourses/ClassSchedule.js
@@ -1,11 +1,25 @@
 import {default as React, PropTypes} from 'react'
 import { RoomFinderLink } from 'components/RoomFinderLink'
 import DaysOfWeekIndicator from './DaysOfWeekIndicator.js'
+import leftPad from 'utils/leftPad'
+import moment from 'moment'
 
 import styles from './MyCourses.css'
 
 const ClassSchedule = ({schedule}) => {
   const { startTime, endTime, days, buildingCode, roomNumber } = schedule
+  const start = {
+    hour: leftPad(startTime, 4, 0).substr(0, 2),
+    minute: leftPad(startTime, 4, 0).substr(2)
+  }
+  const end = {
+    hour: leftPad(endTime, 4, 0).substr(0, 2),
+    minute: leftPad(endTime, 4, 0).substr(2)
+  }
+
+  const startFormatted = moment().hour(start.hour).minute(start.minute).format('h:mm A')
+  const endFormatted = moment().hour(end.hour).minute(end.minute).format('h:mm A')
+
   const location = (schedule) => {
     if (schedule.campus.toLowerCase() === 'burnaby') {
       return <RoomFinderLink building={buildingCode} room={roomNumber} />
@@ -17,7 +31,7 @@ const ClassSchedule = ({schedule}) => {
   return (
     <div className={styles.classSchedule}>
       <DaysOfWeekIndicator days={days} />
-      <span>{startTime} - {endTime}</span>
+      <span>{startFormatted} - {endFormatted}</span>
       {location(schedule)}
     </div>
   )
