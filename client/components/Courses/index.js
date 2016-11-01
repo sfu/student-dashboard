0d452c88fc387e2d90fcbd5b00fd566698d620c6
Graham Ballantyne <grahamb@sfu.ca>
2016-11-01 15:45:29 -0700

    Convert Courses to createClass, pass term as prop

--- client/components/Courses/index.js
+++ client/components/Courses/index.js
@@ -2,24 +2,28 @@ import { default as React, PropTypes } from 'react'
 import Relay from 'react-relay'
 import { Widget } from 'components/Widget'
 import { MyCourses } from 'components/MyCourses'
+import calcTermForDate from 'utils/calcTermForDate'
 
-const _Courses = ({viewer}) => {
-  return(
-    <Widget title="My Courses">
-      <MyCourses courseSchedule={viewer} />
-    </Widget>
-  )
-}
+const _Courses = React.createClass({
+  propTypes: {
+    viewer: PropTypes.object
+  },
 
-_Courses.propTypes = {
-  viewer: PropTypes.object
-}
+  render() {
+    const {viewer} = this.props
+    return (
+      <Widget title="My Courses">
+        <MyCourses courseSchedule={viewer} term={calcTermForDate()}/>
+      </Widget>
+    )
+  }
+})
 
 export const Courses = Relay.createContainer(_Courses, {
   fragments: {
     viewer: () => Relay.QL`
       fragment viewer on ViewerType {
-        ${MyCourses.getFragment('courseSchedule')}
+        ${MyCourses.getFragment('courseSchedule', {term: calcTermForDate()})}
       }
     `
   }
