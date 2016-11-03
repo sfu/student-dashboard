d87f39321e442502dc66c503f1eed62592cf1d04
Graham Ballantyne <grahamb@sfu.ca>
2016-11-03 14:22:58 -0700

    Add term codes to names map to constants

--- client/const/index.js
+++ client/const/index.js
@@ -1,3 +1,4 @@
 export { default as CLASS_TYPES } from './courseTypes'
-export {CALENDAR_DAYS_OF_WEEK, REST_SERVER_DAYS_OF_WEEK} from './days'
+export { CALENDAR_DAYS_OF_WEEK, REST_SERVER_DAYS_OF_WEEK } from './days'
 export { default as TERM_DATES } from './termDates'
+export { default as TERM_CODES_TO_NAMES } from './termCodesToNames'
---
+++ client/const/termCodesToNames.js
@@ -0,0 +1,5 @@
+export default {
+  '1': 'spring',
+  '4': 'summer',
+  '7': 'fall'
+}
