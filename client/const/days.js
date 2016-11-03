ea05899414cad7f798a24202a6bfbddd21a8f8f0
Graham Ballantyne <grahamb@sfu.ca>
2016-11-03 16:19:52 -0700

    Add REST_TO_ABBR_DAYS_MAP const

--- client/const/days.js
+++ client/const/days.js
@@ -1,2 +1,11 @@
 export const REST_SERVER_DAYS_OF_WEEK = ['U', 'M', 'T', 'W', 'R', 'F', 'S']
 export const CALENDAR_DAYS_OF_WEEK = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+export const REST_TO_ABBR_DAYS_MAP = {
+  'U': 'Sun',
+  'M': 'Mon',
+  'T': 'Tue',
+  'W': 'Wed',
+  'R': 'Thurs',
+  'F': 'Fri',
+  'S': 'Sat'
+}
--- client/const/index.js
+++ client/const/index.js
@@ -1,4 +1,8 @@
 export { default as CLASS_TYPES } from './courseTypes'
-export { CALENDAR_DAYS_OF_WEEK, REST_SERVER_DAYS_OF_WEEK } from './days'
+export {
+  CALENDAR_DAYS_OF_WEEK,
+  REST_SERVER_DAYS_OF_WEEK,
+  REST_TO_ABBR_DAYS_MAP
+ } from './days'
 export { default as TERM_DATES } from './termDates'
 export { default as TERM_CODES_TO_NAMES } from './termCodesToNames'
