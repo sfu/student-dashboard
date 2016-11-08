21ec0e0d61c589d485a7b84063afe691edfabafb
Graham Ballantyne <grahamb@sfu.ca>
2016-11-07 16:13:59 -0800

    Implement dynamic routing
    - switch from static to dynamic route config
    - lazy-load page components on navigation

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -1,25 +1,66 @@
 import React from 'react'
-import {Route, IndexRoute} from 'react-router'
-
 import { App } from 'components/App'
-import { Dashboard } from 'pages/Dashboard'
-import { Courses } from 'pages/Courses'
-import { Library } from 'pages/Library'
-import { LibraryBarcode } from 'components/LibraryBarcode'
-import RoomFinder from 'pages/RoomFinder'
 import ViewerQueries from 'queries/ViewerQueries'
 
 const Transit = () => <h1>Transit</h1>
 const Settings = () => <h1>Settings</h1>
 
-export default (
-  <Route path="/" component={App}>
-    <IndexRoute title="Dashboard" component={Dashboard} queries={ViewerQueries} />
-    <Route title="Courses" path="courses" component={Courses} queries={ViewerQueries} />
-    <Route title="Transit" path="transit" component={() => <h1>Transit</h1>} />
-    <Route title="Library" path="library" component={Library} queries={ViewerQueries}/>
-    <Route title="Library Barcode" path="library/barcode" fullScreen={true} component={LibraryBarcode} />
-    <Route title="Room Finder" path="room_finder" component={RoomFinder} />
-    <Route title="Settings" path="settings" component={() => <h1>Settings</h1>} />
-  </Route>
-)
+function errorLoading(err) {
+ console.error('Dynamic page loading failed', err) // eslint-disable-line
+}
+
+function loadRoute(cb) {
+ return (module) => cb(null, module.default)
+}
+
+
+export default {
+  path: '/',
+  component: App,
+  indexRoute: {
+    getComponent(location, cb) {
+      System.import('pages/Dashboard').then(loadRoute(cb)).catch(errorLoading)
+    },
+    title: 'Dashboard',
+    queries: ViewerQueries
+  },
+  childRoutes: [
+    {
+      path: 'courses',
+      title: 'Courses',
+      getComponent(location, cb) {
+        System.import('pages/Courses').then(loadRoute(cb)).catch(errorLoading)
+      },
+      queries: ViewerQueries
+    },
+    {
+      path: 'transit',
+      title: 'Transit',
+      component: Transit,
+      queries: ViewerQueries
+    },
+    {
+      path: 'library',
+      title: 'Library',
+      getComponent(location, cb) {
+        System.import('pages/Library').then(loadRoute(cb)).catch(errorLoading)
+      },
+      queries: ViewerQueries
+    },
+    {
+      path: 'room_finder',
+      title: 'Room Finder',
+      getComponent(location, cb) {
+        System.import('pages/RoomFinder').then(loadRoute(cb)).catch(errorLoading)
+      },
+      queries: ViewerQueries
+    },
+    {
+      path: 'settings',
+      title: 'Settings',
+      component: Settings,
+      queries: ViewerQueries
+    },
+
+  ]
+}
