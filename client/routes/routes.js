59a40e75229aa5ebb089169413f4bb391f63da77
Graham Ballantyne <grahamb@sfu.ca>
2016-11-10 11:46:43 -0800

    Handle loading and errors in relay routes

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import { App } from 'components/App'
+import Loading from 'components/Loading'
+import RelayFetchError from 'components/RelayFetchError'
 import ViewerQueries from 'queries/ViewerQueries'
+
+import styles from 'components/App/App.css'
 
 const Transit = () => <h1>Transit</h1>
 const Settings = () => <h1>Settings</h1>
 
 function errorLoading(err) {
- console.error('Dynamic page loading failed', err) // eslint-disable-line
+  console.error('Dynamic page loading failed', err) // eslint-disable-line
 }
 
 function loadRoute(cb) {
- return (module) => cb(null, module.default)
+  return (module) => cb(null, module.default)
 }
 
+const render = ({done, error, props, retry, element}) => {  // eslint-disable-line
+  if (!done && !error && !props) {
+    // data is being fetched, show loading indicator
+    return (<div className={styles.loadingWrapper}><Loading /></div>)
+  } else if (!done && error) {
+    // error returned from the server
+    return (
+      <div className={styles.fetchError}>
+        <RelayFetchError error={error} retry={retry} />
+      </div>
+    )
+  } else if (done && props) {
+    return React.cloneElement(element, {...props})
+  }
+}
 
 export default {
   path: '/',
@@ -22,7 +41,8 @@ export default {
       System.import('pages/Dashboard').then(loadRoute(cb)).catch(errorLoading)
     },
     title: 'Dashboard',
-    queries: ViewerQueries
+    queries: ViewerQueries,
+    render
   },
   childRoutes: [
     {
@@ -31,13 +51,13 @@ export default {
       getComponent(location, cb) {
         System.import('pages/Courses').then(loadRoute(cb)).catch(errorLoading)
       },
-      queries: ViewerQueries
+      queries: ViewerQueries,
+      render
     },
     {
       path: 'transit',
       title: 'Transit',
-      component: Transit,
-      queries: ViewerQueries
+      component: Transit
     },
     {
       path: 'library',
@@ -45,21 +65,20 @@ export default {
       getComponent(location, cb) {
         System.import('pages/Library').then(loadRoute(cb)).catch(errorLoading)
       },
-      queries: ViewerQueries
+      queries: ViewerQueries,
+      render
     },
     {
       path: 'room_finder',
       title: 'Room Finder',
       getComponent(location, cb) {
         System.import('pages/RoomFinder').then(loadRoute(cb)).catch(errorLoading)
-      },
-      queries: ViewerQueries
+      }
     },
     {
       path: 'settings',
       title: 'Settings',
-      component: Settings,
-      queries: ViewerQueries
+      component: Settings
     },
 
   ]
