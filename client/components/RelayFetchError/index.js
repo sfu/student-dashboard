32941293ddbb3e4e93b519856db9fbae59449f47
Graham Ballantyne <grahamb@sfu.ca>
2016-11-10 11:43:57 -0800

    Add RelayFetchError component
    Displays an error message and an optional rety button.

---
+++ client/components/RelayFetchError/index.js
@@ -0,0 +1,24 @@
+import { default as React, PropTypes } from 'react'
+import styles from './RelayFetchError.css'
+
+const RelayFetchError = ({error, retry}) => {
+  console.error(error) // eslint-disable-line
+  return (
+    <div className={styles.fetchError}>
+      <h1>Oh, Snap!</h1>
+      <p>Something went wrong while fetching your data from the server.</p>
+      {retry && <button className={styles.retryButton} onClick={retry}>Try Again</button>}
+    </div>
+  )
+}
+
+RelayFetchError.defaultProps = {
+  retry: null
+}
+
+RelayFetchError.propTypes = {
+  error: PropTypes.object.isRequired,
+  retry: PropTypes.func
+}
+
+export default RelayFetchError
