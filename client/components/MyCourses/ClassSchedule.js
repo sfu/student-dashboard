4fa0b163b22baac0c2fc67bb81b19300d851b569
Graham Ballantyne <grahamb@sfu.ca>
2016-11-14 14:07:19 -0800

    Handle case of reciving null meeting days and times for a class

--- client/components/MyCourses/ClassSchedule.js
+++ client/components/MyCourses/ClassSchedule.js
@@ -23,7 +23,7 @@ const ClassSchedule = ({schedule}) => {
   const endMoment = moment().hour(end.hour).minute(end.minute)
 
   const location = (schedule) => {
-    const location = schedule.campus.toLowerCase() === 'burnaby' ?
+    const location = schedule.campus.toLowerCase() === 'burnaby' && buildingCode && roomNumber ?
       <RoomFinderLink building={buildingCode} room={roomNumber} /> :
       <span>{schedule.buildingCode} {schedule.roomNumber}</span>
 
@@ -34,12 +34,12 @@ const ClassSchedule = ({schedule}) => {
     )
   }
 
-  const classDays = days.split('').map(day => REST_TO_ABBR_DAYS_MAP[day]).join(', ')
+  const classDays = days ? days.split('').map(day => REST_TO_ABBR_DAYS_MAP[day]).join(', ') : 'No In-Person Meetings'
 
   return (
     <div className={styles.classSchedule}>
       <span className={styles.classDays}>{classDays}</span>
-      <span className={styles.classTimes}>{formatTimeRange(startMoment, endMoment)}</span>
+      <span className={styles.classTimes}>{startTime && endTime ? formatTimeRange(startMoment, endMoment) : null}</span>
       {location(schedule)}
     </div>
   )
