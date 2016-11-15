61bf87520ca9c66a9dbbb51c0a04727cf7198413
Graham Ballantyne <grahamb@sfu.ca>
2016-11-15 14:57:52 -0800

    Convert <Header> to stateless component
    Replace showNavGrid state with showNav and toggleHeaderNav passed in as props from above

--- client/components/Header/index.js
+++ client/components/Header/index.js
@@ -8,45 +8,31 @@ import Menu from './menu.svg'
 import styles from './Header.css'
 import cx from 'classnames'
 
-export const Header = React.createClass({
-  propTypes: {
-    title: PropTypes.string.isRequired
-  },
+export const Header = ({ title, showNav, toggleHeaderNav }) => {
+  const menuToggleClassNames = showNav ? cx(styles.navToggle, styles.navToggleActive) : styles.navToggle
+  return (
+    <div className={styles.headerContainer}>
+      <header className={styles.header}>
+        <img className={styles.snapLogo} alt="SFU Snap Logo" src={snapLogo} height={50} width={50} />
+        <button className={menuToggleClassNames} onClick={toggleHeaderNav}><h1 className={styles.title}>{title}</h1></button>
+        <button type="button" name="Toggle Menu" className={menuToggleClassNames} onClick={toggleHeaderNav}>
+          <Menu />
+        </button>
+      </header>
 
-  getInitialState() {
-    return {
-      showNavGrid: false
-    }
-  },
+      <Collapse
+        isOpened={showNav}
+        springConfig={presets.stiff}
+      >
+        <HeaderNavGrid />
+        <LogoutButton />
+      </Collapse>
+    </div>
+  )
+}
 
-  toggleNavGrid(ev) {
-    ev.preventDefault()
-    this.setState({
-      showNavGrid: !this.state.showNavGrid
-    })
-  },
-
-  render() {
-    const menuToggleClassNames = this.state.showNavGrid ? cx(styles.navToggle, styles.navToggleActive) : styles.navToggle
-    const { title } = this.props
-    return (
-      <div className={styles.headerContainer}>
-        <header className={styles.header}>
-          <img className={styles.snapLogo} alt="SFU Snap Logo" src={snapLogo} height={50} width={50} />
-          <button className={menuToggleClassNames} onClick={this.toggleNavGrid}><h1 className={styles.title}>{title}</h1></button>
-          <button type="button" name="Toggle Menu" className={menuToggleClassNames} onClick={this.toggleNavGrid}>
-            <Menu />
-          </button>
-        </header>
-
-        <Collapse
-          isOpened={this.state.showNavGrid}
-          springConfig={presets.stiff}
-        >
-          <HeaderNavGrid />
-          <LogoutButton />
-        </Collapse>
-      </div>
-    )
-  }
-})
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  showNav: PropTypes.bool.isRequired,
+  toggleHeaderNav: PropTypes.func.isRequired
+}
