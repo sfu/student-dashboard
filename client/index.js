d53c63a0bfc76a09c7bebbd4137ab9390cd3d804
Graham Ballantyne <grahamb@sfu.ca>
2016-11-15 14:56:29 -0800

    Reduxify app
    - Move <Router> into index, disable HMR for now, create redux store and and wrap <Router> in <Provider>
    
    - Add a browserHistory listener to handle toggling nav to hidden if it is shown when route transitions
    
    TODO: either get HMR working again, or remove the commented-out cruft

--- client/index.js
+++ client/index.js
@@ -2,9 +2,18 @@ import 'babel-polyfill'
 import './routerHotLoadPatch'
 import Relay from 'react-relay'
 import React from 'react'
-import {AppContainer} from 'react-hot-loader'
+// import {AppContainer} from 'react-hot-loader'
 import ReactDOM from 'react-dom'
-import Renderer from './routes/router'
+// import Renderer from './routes/router'
+import {Router, browserHistory, applyRouterMiddleware} from 'react-router'
+import useRelay from 'react-router-relay'
+import routes from './routes/routes'
+
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { header } from './reducers'
+
+const store = createStore(combineReducers({ header }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const RootElement = document.getElementById('sorry')
 
@@ -19,20 +28,62 @@ if (process.env.NODE_ENV !== 'production') {
   RelayNetworkDebug.init()
 }
 
-const render = () => {
-  ReactDOM.render(<AppContainer>{Renderer}</AppContainer>, RootElement)
-}
+browserHistory.listen(() => {
+  if (store.getState().header.showNav) {
+    store.dispatch({ type: 'TOGGLE_HEADER_NAV' })
+  }
+})
 
-render()
+ReactDOM.render(
+  <Provider store={store}>
+    <Router
+      history={browserHistory}
+      render={applyRouterMiddleware(useRelay)}
+      environment={Relay.Store}
+      routes={routes}
+    />
+  </Provider>,
+  RootElement
+)
 
-if (module.hot) {
-  module.hot.accept('./routes/router', () => {
-    const NextRootContainer = require('./routes/router').default
-    render(
-      <AppContainer>
-         {NextRootContainer}
-      </AppContainer>,
-      RootElement
-    )
-  })
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// const render = () => {
+//   ReactDOM.render(<AppContainer>{Renderer}</AppContainer>, RootElement)
+// }
+//
+// render()
+//
+// if (module.hot) {
+//   module.hot.accept('./routes/router', () => {
+//     const NextRootContainer = require('./routes/router').default
+//     render(
+//       <AppContainer>
+//          {NextRootContainer}
+//       </AppContainer>,
+//       RootElement
+//     )
+//   })
+// }
