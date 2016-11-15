41dcd99f5b5d6dc4701cb5b0d896e931717d233d
Graham Ballantyne <grahamb@sfu.ca>
2016-11-15 14:54:04 -0800

    Change App import in routes

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { App } from 'components/App'
+import App from 'components/App'
 import Loading from 'components/Loading'
 import RelayFetchError from 'components/RelayFetchError'
 import ViewerQueries from 'queries/ViewerQueries'
