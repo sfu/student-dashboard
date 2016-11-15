a28dd542dcb8a73949540d205d0233e7d31e6dae
Graham Ballantyne <grahamb@sfu.ca>
2016-11-15 14:53:48 -0800

    Wire up App to redux store with connect()

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -1,19 +1,34 @@
 import {default as React, PropTypes} from 'react'
-
+import { connect } from 'react-redux'
 import {Header} from 'components/Header'
 
 import 'normalize.css/normalize.css'
 import 'styles/global.css'
 import styles from './App.css'
 
+const mapStateToProps = (state) => {
+  return {
+    showNav: state.header.showNav
+  }
+}
 
-export const App = ({children}) => {
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleHeaderNav: () => {
+      dispatch({
+        type: 'TOGGLE_HEADER_NAV'
+      })
+    }
+  }
+}
+
+const App = ({ children, showNav, toggleHeaderNav }) => {
   const childProps = children.props.routerProps || children.props
   const { fullScreen } = childProps.route
   return (
     <div>
       <div className={styles.app}>
-        { fullScreen ? null : <Header title={childProps.route.title} /> }
+        { fullScreen ? null : <Header showNav={showNav} toggleHeaderNav={toggleHeaderNav} title={childProps.route.title} /> }
         <div className={fullScreen ? '' : styles.widgets}>
           {children}
         </div>
@@ -23,5 +38,9 @@ export const App = ({children}) => {
 }
 
 App.propTypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  showNav: PropTypes.bool.isRequired,
+  toggleHeaderNav: PropTypes.func.isRequired
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
