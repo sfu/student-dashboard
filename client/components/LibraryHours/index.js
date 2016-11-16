9c1078165e729544eb5e9e0f35679440c07327e1
Graham Ballantyne <grahamb@sfu.ca>
2016-11-16 15:48:50 -0800

    Refactor LibraryHours into a stateless compoonent using redux store
    - move fetching logic into redux actions
    - handle initial fetch on route change (in client/index.js - bit of a hack, would have preferred to do it on the /library route's onEnter hook)

--- client/components/LibraryHours/index.js
+++ client/components/LibraryHours/index.js
@@ -1,63 +1,40 @@
-import React from 'react'
-import axios from 'axios'
+import { default as React, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
 import styles from './LibraryHours.css'
 
-const LibraryHours = React.createClass({
-  libraryHoursUrl: 'https://www.sfu.ca/bin/library-hours.json',
-  campusMapping: {
-    'Bennett Library': 'Burnaby Campus',
-    'Fraser Library': 'Surrey Campus',
-    'Belzberg Library': 'Vancouver Campus'
-  },
-
-  propTypes: {},
-
-  getInitialState() {
-    return {
-      fetching: true,
-      error: false,
-      hours: []
-    }
-  },
-
-  componentDidMount() {
-    axios.get(this.libraryHoursUrl).then(({data}) => {
-      // augment `data` with a campus field using `this.campusMapping`
-      this.setState({
-        hours: data.map((item) => ({...item, campus: this.campusMapping[item.location]})),
-        fetching: false
-      })
-    }).catch((error) => {
-      this.setState({
-        fetching: false,
-        error
-      })
-    })
-  },
-
-  render() {
-    return (
-      <div className={styles.libraryHours}>
-        {
-          this.state.fetching ? <p>Checking Libary Hours...</p> :
-          <ul className={styles.libraryHoursList}>
-            {this.state.hours.map((location, index) => (
-              <li className={styles.libraryHoursListItem} key={index}>
-                <div className={styles.col1}>
-                  <h3 className={styles.libraryLocation}>{location.location}</h3>
-                  <span>{location.campus}</span>
-                </div>
-                <div className={styles.col2}>
-                  <span className={styles.openClosed}>{location.in_range ? 'Open' : 'Closed'}</span>
-                  <span>{location.open_time} - {location.close_time}</span>
-                </div>
-              </li>
-            ))}
-          </ul>
-        }
-      </div>
-    )
+const mapStateToProps = state => {
+  return {
+    hours: state.library.hours
   }
-})
+}
 
-export default LibraryHours
+const LibraryHours = ({hours}) => {
+  return (
+    <div className={styles.libraryHours}>
+      {
+        hours.fetching  ? <p>Checking Libary Hours...</p> :
+        <ul className={styles.libraryHoursList}>
+          {hours.data.map((location, index) => (
+            <li className={styles.libraryHoursListItem} key={index}>
+              <div className={styles.col1}>
+                <h3 className={styles.libraryLocation}>{location.location}</h3>
+                <span>{location.campus}</span>
+              </div>
+              <div className={styles.col2}>
+                <span className={styles.openClosed}>{location.in_range ? 'Open' : 'Closed'}</span>
+                <span>{location.open_time} - {location.close_time}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      }
+    </div>
+  )
+}
+
+LibraryHours.propTypes = {
+  hours: PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps)(LibraryHours)
