3d8a2f2b0a5cb25b770a04c38883d6b6bddcf2fe
Graham Ballantyne <grahamb@sfu.ca>
2016-11-15 16:29:28 -0800

    Clean up header reducer

--- client/reducers/header.js
+++ client/reducers/header.js
@@ -1,13 +1,10 @@
 export default (state = { showNav: false }, action) => {
-  console.log({state, action})
   switch (action.type) {
     case 'TOGGLE_HEADER_NAV':
-      const nextState = {
+      return {
         ...state,
         showNav: !state.showNav
       }
-      console.log(nextState)
-      return nextState
     default:
       return state
   }
