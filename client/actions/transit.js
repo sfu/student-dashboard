41d0440e4be32fe790a400d2e3c7f1142fb42e35
Graham Ballantyne <grahamb@sfu.ca>
2016-11-25 15:58:18 -0800

    Keep old stops when fetching more

--- client/actions/transit.js
+++ client/actions/transit.js
@@ -7,19 +7,21 @@ export const FETCH_STOPS_ERROR = 'FETCH_STOPS_ERROR'
 export const TOGGLE_CURRENT_LOCATION_ON_MAP = 'TOGGLE_CURRENT_LOCATION_ON_MAP'
 export const SET_SELECTED_STOP = 'SET_SELECTED_STOP'
 
-export const fetchStops = (coords, radius = 600) => {
-  return (dispatch) => {
 export const FETCH_SCHEDULE_FOR_BUS_STOP_START = 'FETCH_SCHEDULE_FOR_BUS_STOP_START'
 export const FETCH_SCHEDULE_FOR_BUS_STOP_SUCCESS = 'FETCH_SCHEDULE_FOR_BUS_STOP_SUCCESS'
 export const FETCH_SCHEDULE_FOR_BUS_STOP_ERROR = 'FETCH_SCHEDULE_FOR_BUS_STOP_ERROR'
 export const FETCH_SCHEDULE_FOR_BUS_STOP = 'FETCH_SCHEDULE_FOR_BUS_STOP'
 
+
+export const fetchStops = (coords, radius) => {
+  return (dispatch, getState) => {
     dispatch(fetchStopsStart())
     const latitude = parseFloat(coords.latitude).toFixed(5)
     const longitude =  parseFloat(coords.longitude).toFixed(5)
     const STOPS_URL = `/translink/stops?lat=${latitude}&long=${longitude}&radius=${radius}`
     return axios.get(STOPS_URL).then((response) => {
-      dispatch(fetchStopsSuccess(response.data))
+      const nextState = uniqBy(getState().transit.stops.concat(response.data), 'StopNo')
+      dispatch(fetchStopsSuccess(nextState))
     }).catch((error) => {
       dispatch(fetchStopsError(error))
     })
