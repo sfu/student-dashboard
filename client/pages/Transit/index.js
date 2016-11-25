8e0d1795948a74f3761ab13a4dc331e785aedf29
Graham Ballantyne <grahamb@sfu.ca>
2016-11-25 15:55:20 -0800

    Add BusSchedules to Transit page

--- client/pages/Transit/index.js
+++ client/pages/Transit/index.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import TransitMap from 'components/TransitMap'
+import BusSchedules from 'components/BusSchedules'
 
 const mapStateToProps = state => ({
   transit: state.transit
 })
 
-const Transit = () => {
+const Transit = ({transit}) => {
   return (
     <div>
       <TransitMap />
+      {transit.selectedStop &&
+        <BusSchedules
+          selectedStop={transit.selectedStop}
+          schedules={transit.schedulesForSelectedStop}
+        />
+      }
     </div>
 
   )
+}
+
+Transit.propTypes = {
+  transit: PropTypes.object.isRequired
 }
 
 export default connect(mapStateToProps)(Transit)
