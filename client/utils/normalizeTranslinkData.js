2c786d1a69ff133e63ab74d392c81a4185c5393a
Graham Ballantyne <grahamb@sfu.ca>
2016-11-26 16:00:13 -0800

    Add normalizeTranslinkData util function

---
+++ client/utils/normalizeTranslinkData.js
@@ -0,0 +1,14 @@
+const generateSchedulesForRoute = (s) => {
+  const destinations = [...new Set(s.Schedules.map(s => s.Destination))]
+  return destinations.map(d => {
+    return {
+      ...s,
+      Schedules: s.Schedules.filter(x => x.Destination === d)
+    }
+  })
+}
+
+export default data => {
+  return data.map(generateSchedulesForRoute)
+             .reduce(((a, b) => a.concat(b)), [])
+}
