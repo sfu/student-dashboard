34a602b349f7c6834d46c2ce46cf8d9bdb706c01
Graham Ballantyne <grahamb@sfu.ca>
2016-11-30 14:05:09 -0800

    TEMP: Preload preferences from localStorage
    TODO: Replace with loading from server

--- client/configureStore.js
+++ client/configureStore.js
@@ -1,6 +1,6 @@
 /*eslint import/namespace: [2, { allowComputed: true }]*/
 
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import * as reducers from './reducers'
 
@@ -8,11 +8,25 @@ const reducerObj = {}
 
 Object.keys(reducers).forEach(r => reducerObj[r] = reducers[r])
 
+var hydratedState = {}
+
+try {
+  const preferences = JSON.parse(localStorage.getItem('preferences'))
+  if (preferences) {
+    hydratedState['preferences'] = preferences
+  }
+} catch (e) {
+  console.error(e) // eslint-disable-line no-console
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export default () => {
   return createStore(
     combineReducers(reducerObj),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunkMiddleware)
+    hydratedState,
+    composeEnhancers(
+      applyMiddleware(thunkMiddleware),
+    )
   )
 }
