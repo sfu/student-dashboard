4a239956a6fb4fc276ba0fc48c6f79b6fae144bf
Graham Ballantyne <grahamb@sfu.ca>
2016-12-13 12:06:41 -0800

    Add DashboardTransitBookmarks component

---
+++ client/components/DashboardTransitBookmarks/index.js
@@ -0,0 +1,57 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import formatTime from 'utils/formatTime'
+import { fetchSchedulesForBookmarks } from 'actions/transit'
+
+import DashboardTransitBookmarkRow from 'components/DashboardTransitBookmarkRow'
+import styles from './DashboardTransitBookmarks.css'
+
+const renderRows = (schedules) => {
+  const rows =Object.keys(schedules).map((b, i) => {
+    return <DashboardTransitBookmarkRow key={i} busRoute={schedules[b]} />
+  })
+
+  return rows.length ? rows : null
+}
+
+
+class DashboardTransitBookmarks extends Component {
+  refresh() {
+    this.props.dispatch(fetchSchedulesForBookmarks())
+  }
+
+  render() {
+    const {
+      transitBookmarksSchedules,
+      transitBookmarksSchedulesFetchedAt,
+     } = this.props.transit
+    return (
+      <div className={styles.container}>
+        {
+          transitBookmarksSchedulesFetchedAt && (
+          <div className={styles.scheduleControls}>
+            <p className={styles.fetchedAt}>
+              Predictions as of {formatTime(transitBookmarksSchedulesFetchedAt)}
+            </p>
+            <button className={styles.refreshButton} onClick={() => {this.refresh()}}>Refresh</button>
+          </div>
+          )
+        }
+        {renderRows(transitBookmarksSchedules)}
+      </div>
+    )
+  }
+}
+
+DashboardTransitBookmarks.propTypes = {
+  transit: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => {
+  return {
+    transit: state.transit
+  }
+}
+
+export default connect(mapStateToProps)(DashboardTransitBookmarks)
