016455238d6f09cee4393c7d0630cb0170d637f8
Graham Ballantyne <grahamb@sfu.ca>
2016-12-15 11:52:54 -0800

    Fix default preferences value in migration

--- server/db/migrations/20161215103944_addPreferencesTextColumn.js
+++ server/db/migrations/20161215103944_addPreferencesTextColumn.js
@@ -2,7 +2,7 @@ const table = 'users'
 
 exports.up = function(knex) {
   return knex.schema.table(table, t => {
-    t.text('preferences_text').defaultTo('[]').comment(`The user''s preferences serialized as JSON text`)
+    t.text('preferences_text').defaultTo('{}').comment(`The user''s preferences serialized as JSON text`)
   })
 }
 
