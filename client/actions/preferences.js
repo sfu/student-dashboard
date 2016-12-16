322d9e3930902d99cf11d7993a581f23b14cab73
Graham Ballantyne <grahamb@sfu.ca>
2016-12-16 14:07:25 -0800

    Refactor preferences actions & reducers for server-backing

--- client/actions/preferences.js
+++ client/actions/preferences.js
@@ -1,12 +1,54 @@
-export const GET_PREFERENCES = 'GET_PREFERENCES'
-export const SET_PREFERENCE = 'SET_PREFERENCE'
+import axios from 'axios'
 
-export const getPreferences = () => ({ type: GET_PREFERENCES })
+export const SYNC_PREFERENCES_START = 'SYNC_PREFERENCES_START'
+export const SYNC_PREFERENCES_SUCCESS = 'SYNC_PREFERENCES_SUCCESS'
+export const SYNC_PREFERENCES_ERROR = 'SYNC_PREFERENCES_ERROR'
+export const SET_PREFERENCES = 'SET_PREFERENCES'
 
-export const setPreference = (preference, value) => {
+const PREFERENCES_URL = '/api/v1/users/self/preferences'
+
+export const setPreferences = preferences => {
   return {
-    type: SET_PREFERENCE,
-    preference,
-    value
+    type: SET_PREFERENCES,
+    preferences
+  }
+}
+
+export const syncPreferencesStart = () => {
+  return {
+    type: SYNC_PREFERENCES_START
+  }
+}
+
+export const syncPreferencesSuccess = () => {
+  return {
+    type: SYNC_PREFERENCES_SUCCESS
+  }
+}
+
+export const syncPreferencesError = error => {
+  return {
+    type: SYNC_PREFERENCES_ERROR,
+    error
+  }
+}
+
+export const updatePreference = (key, value) => {
+  return (dispatch, getState) => {
+    const currentPreferences = getState().preferences.preferenceData
+    // optimistic update
+    dispatch(setPreferences([...currentPreferences, {key, value}]))
+    dispatch(syncPreferencesStart())
+    return axios({
+      method: 'POST',
+      url: PREFERENCES_URL,
+      data: { key, value },
+    }).then(() => {
+      dispatch(syncPreferencesSuccess())
+    }).catch(error => {
+      // reset from optimistic update
+      dispatch(setPreferences(currentPreferences))
+      dispatch(syncPreferencesError(error.response.data))
+    })
   }
 }
--- client/reducers/preferences.js
+++ client/reducers/preferences.js
@@ -1,26 +1,52 @@
 import {
-  GET_PREFERENCES,
-  SET_PREFERENCE
+  SYNC_PREFERENCES_START,
+  SYNC_PREFERENCES_SUCCESS,
+  SYNC_PREFERENCES_ERROR,
+  SET_PREFERENCES
 } from '../actions/preferences'
 
+const BOOTSTRAP = (window && window.STATE_BOOTSTRAP && window.STATE_BOOTSTRAP.preferences) || {}
+
 export const DEFAULT = {
-  timeFormat: '12h'
+  syncingPreferences: false,
+  syncPreferencesError: null,
+  preferenceData: {
+    timeFormat: '12h',
+    ...BOOTSTRAP
+  }
 }
 
 export default (state = DEFAULT, action) => {
   const { type } = action
   switch (type) {
-    case GET_PREFERENCES:
-      return state
-
-    case SET_PREFERENCE: {
-      const { preference, value } = action
-
+    case SYNC_PREFERENCES_START:
       return {
         ...state,
-        [preference]: value
+        syncingPreferences: true,
+        syncPreferencesError: null
       }
-    }
+
+    case SYNC_PREFERENCES_SUCCESS:
+      return {
+        ...state,
+        syncingPreferences: false,
+        syncPreferencesError: null
+      }
+
+    case SYNC_PREFERENCES_ERROR:
+      return {
+        ...state,
+        syncingPreferences: false,
+        syncPreferencesError: action.error
+      }
+
+    case SET_PREFERENCES:
+      return {
+        ...state,
+        syncingPreferences: false,
+        syncPreferencesError: null,
+        preferenceData: action.preferences
+      }
 
     default:
       return state
