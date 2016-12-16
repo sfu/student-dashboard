1d5090b84cf4d047f6026dc64ddc3b209df2bb33
Graham Ballantyne <grahamb@sfu.ca>
2016-12-16 15:43:41 -0800

    Add Time Format to Settings page

--- client/pages/Settings/index.js
+++ client/pages/Settings/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import { Widget } from 'components/Widget'
+import SettingsSelect from 'components/SettingsSelect'
 
 const Settings = () => {
   return (
     <div>
-      <h1>Settings</h1>
+      <Widget title="General Settings">
+        <div style={{ margin: '1em' }}>
+          <SettingsSelect
+            label="Time Format"
+            preferenceKey="timeFormat"
+            options={[
+              { value: '12h', text: '12-hour (1:00 PM)'},
+              { value: '24h', text: '24-hour (13:00)'}
+            ]}
+          />
+        </div>
+      </Widget>
     </div>
   )
 }
 
-Settings.propTypes = {
-
-}
+Settings.propTypes = {}
 
 export default Settings
