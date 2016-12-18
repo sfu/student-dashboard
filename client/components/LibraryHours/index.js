a0765803e583e485cf45f9c078dc189ecbc86446
Graham Ballantyne <grahamb@sfu.ca>
2016-12-17 23:43:01 -0800

    Update LibraryHours to use use timeFormat

--- client/components/LibraryHours/index.js
+++ client/components/LibraryHours/index.js
@@ -1,16 +1,43 @@
 import React, { PropTypes }  from 'react'
 import { connect } from 'react-redux'
+import { TIME_SEPARATOR } from 'const/timeFormat'
+import leftPad from 'utils/leftPad'
 
 import styles from './LibraryHours.css'
 
 const mapStateToProps = state => {
   return {
-    hours: state.library.hours
+    hours: state.library.hours,
+    timeFormat: state.preferences.preferenceData.timeFormat
   }
 }
 
-const LibraryHours = ({hours}) => {
-  return (
+const formatLibraryHours = (str, format = '12h') => {
+  const arr = str.trim().split(':')
+  let hour = parseInt(arr[0])
+  const minute = arr[1].substr(0,2)
+  const meridiem = arr[1].substr(2,2)
+
+  if (format === '24h') {
+    if (meridiem === 'PM') {
+      hour = hour + 12
+    } else {
+      hour = leftPad(hour,2, '0')
+    }
+    return `${hour}:${minute}`
+  } else {
+    return `${hour}:${minute} ${meridiem}`
+  }
+}
+
+const LibraryHours = ({ hours, timeFormat }) => {
+
+  const openHours = location => {
+    return location.open_all_day ? 'Open 24 Hours' :
+      `${formatLibraryHours(location.open_time, timeFormat)} ${TIME_SEPARATOR} ${formatLibraryHours(location.close_time, timeFormat)}`
+  }
+
+  return  (
     <div className={styles.libraryHours}>
       {
         hours.fetching  ? <p>Checking Libary Hours...</p> :
@@ -23,7 +50,9 @@ const LibraryHours = ({hours}) => {
               </div>
               <div className={styles.col2}>
                 <span className={styles.openClosed}>{location.in_range ? 'Open' : 'Closed'}</span>
-                <span>{location.open_time} - {location.close_time}</span>
+                <span>
+                  {openHours(location)}
+                </span>
               </div>
             </li>
           ))}
@@ -34,7 +63,8 @@ const LibraryHours = ({hours}) => {
 }
 
 LibraryHours.propTypes = {
-  hours: PropTypes.object.isRequired
+  hours: PropTypes.object.isRequired,
+  timeFormat: PropTypes.string.isRequired
 }
 
 export default connect(mapStateToProps)(LibraryHours)
