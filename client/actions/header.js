d8310c66929a381d9ca8859e2dfceb8a9e2284f9
Graham Ballantyne <grahamb@sfu.ca>
2016-12-23 17:02:57 -0800

    Fire GA event on header nav toggle

--- client/actions/header.js
+++ client/actions/header.js
@@ -1,5 +1,16 @@
+import GAEvent from 'utils/GAEvent'
+
+export const TOGGLE_HEADER_NAV = 'TOGGLE_HEADER_NAV'
+
 export const toggleHeaderNav = () => {
-  return {
-    type: 'TOGGLE_HEADER_NAV',
+  return (dispatch, getState) => {
+    dispatch({
+      type: TOGGLE_HEADER_NAV,
+    })
+    GAEvent({
+      category: 'Header',
+      action: TOGGLE_HEADER_NAV,
+      label: getState().header.showNav ? 'close' : 'open'
+    })
   }
 }
--- client/reducers/header.js
+++ client/reducers/header.js
@@ -1,10 +1,12 @@
+import { TOGGLE_HEADER_NAV } from 'actions/header'
+
 export const DEFAULT = {
   showNav: false
 }
 
 export default (state = DEFAULT, action) => {
   switch (action.type) {
-    case 'TOGGLE_HEADER_NAV':
+    case TOGGLE_HEADER_NAV:
       return {
         ...state,
         showNav: !state.showNav
