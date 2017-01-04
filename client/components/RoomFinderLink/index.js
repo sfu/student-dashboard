d2eb5380ace63ce7dbce6c5db0b061644def7783
Graham Ballantyne <grahamb@sfu.ca>
2017-01-03 17:06:53 -0800

    Track RoomFinderLink clicks

--- client/components/RoomFinderLink/index.js
+++ client/components/RoomFinderLink/index.js
@@ -1,11 +1,20 @@
 import React, { PropTypes }  from 'react'
 import { Link } from 'react-router'
+import ReactGA from 'react-ga'
 
 export const RoomFinderLink = ({building, room}) =>
   <Link
     to={{
       pathname: '/room_finder',
       query: { building, room }
+    }}
+    onClick={(ev) => {
+      ev.stopPropagation()
+      ReactGA.event({
+        category: 'RoomFinderLink',
+        action: 'click',
+        label: `${building}_${room}`
+      })
     }}
   >
     {building} {room}
