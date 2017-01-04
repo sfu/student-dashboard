552518a1d11236c450c834968ce58626b293838c
Graham Ballantyne <grahamb@sfu.ca>
2017-01-03 17:08:00 -0800

    Handle NavTile GA click event directly on Link onClick

--- client/components/NavTile/index.js
+++ client/components/NavTile/index.js
@@ -1,17 +1,11 @@
-/* eslint-disable
-  jsx-a11y/click-events-have-key-events,
-  jsx-a11y/no-static-element-interactions,
-  jsx-a11y/onclick-has-role,
-  jsx-a11y/onclick-has-focus
-*/
-
 import React, { PropTypes }  from 'react'
 import ReactGA from 'react-ga'
 import { Link } from 'react-router'
 import styles from './NavTile.css'
 
 const NavTile = ({LinkTo, Title, Icon, OnlyActiveOnIndex = false, placement }) => {
-  const sendEvent = () => {
+  const sendEvent = (ev) => {
+    ev.stopPropagation()
     ReactGA.event({
       category: `NavTile_${placement}`,
       action: 'click',
@@ -19,8 +13,8 @@ const NavTile = ({LinkTo, Title, Icon, OnlyActiveOnIndex = false, placement }) =
     })
   }
   return (
-    <div onClick={sendEvent} className={styles.tileContainer}>
-      <Link to={LinkTo} activeClassName={styles.active} onlyActiveOnIndex={OnlyActiveOnIndex}>
+    <div className={styles.tileContainer}>
+      <Link onClick={sendEvent} to={LinkTo} activeClassName={styles.active} onlyActiveOnIndex={OnlyActiveOnIndex}>
         <div className={styles.tile}>
           <div className={styles.icon}><Icon /></div>
           <div className={styles.title}>{Title}</div>
