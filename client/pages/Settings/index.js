2dcca252788ee432a5a3a6a3a464ccad8c2d7107
Graham Ballantyne <grahamb@sfu.ca>
2017-01-18 10:45:51 -0800

    Add about box and LogoutButton to settings page

--- client/pages/Settings/index.js
+++ client/pages/Settings/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Widget } from 'components/Widget'
 import SettingsSelect from 'components/SettingsSelect'
+import LogoutButton from 'components/LogoutButton'
+import styles from './Settings.css'
 
 const Settings = () => {
   return (
     <div>
       <Widget title="General Settings">
-        <div style={{ margin: '1em' }}>
+        <div className={styles.container}>
           <SettingsSelect
             label="Time Format"
             preferenceKey="timeFormat"
@@ -17,6 +19,22 @@ const Settings = () => {
           />
         </div>
       </Widget>
+
+      <Widget title="About SFU Snap">
+        <div className={styles.container}>
+          <p className={styles.aboutText}>
+            SFU Snap was created by students, for students. It provides you
+            with the information you need to easliy plan your campus experience
+            in a snap. Access your personalized course schedule, find room
+            locations,consult transit schedules, and manage your library.
+          </p>
+          <p className={styles.copyright}>
+            © Copyright Simon Fraser University
+          </p>
+          { process.env.BUILD && <p className={styles.build}>Build: {process.env.BUILD}</p>}
+        </div>
+      </Widget>
+      <LogoutButton />
     </div>
   )
 }
