cc7b86edb6f9031a17ad9908227560ecea622620
Graham Ballantyne <grahamb@sfu.ca>
2017-05-31 11:18:15 -0700

    Add unique constraint on username

---
+++ server/db/migrations/20170531104300_users_username_unique.js
@@ -0,0 +1,13 @@
+const table = 'users'
+
+exports.up = function(knex) {
+  return knex.schema.alterTable(table, (t) => {
+    t.unique('username')
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.alterTable(table, (t) => {
+    t.dropUnique('username')
+  })
+}
